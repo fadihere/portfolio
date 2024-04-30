c7b89c997d6b8f558dcf1b69b7bf2a3d32684917
fadihere <fahadsaleem303@gmail.com>
2024-04-30 22:53:04 +0500

    run dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,105 +1,178 @@
-
-import {Head, Html, Main, NextScript} from 'next/document'
-import Script from 'next/script';
-import React from "react";
-
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-	return (
-		<Html lang="en">
-
-            <Head>
-                {/*
+  return (
+    <Html lang="en">
+      <Head>
+        {/*
 				Author: Muhammad Fiaz
                  GitHub: Visit the GitHub repository at https://github.com/muhammad-fiaz/portfolio/ for more details.
                  license: MIT License
                   */}
-                <meta charSet="utf-8"/>
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
-                <title>Muhammad Fiaz</title>
-                <meta name="description"
-                      content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AL/ML, and Design. Join my journey."/>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+        <title>Muhammad Fiaz</title>
+        <meta
+          name="description"
+          content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AL/ML, and Design. Join my journey."
+        />
 
-                {/*These are need for PWA*/}
-                <meta name="apple-mobile-web-app-capable" content="yes"/>
-                <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
-                <meta name="apple-mobile-web-app-title" content="Muhammad Fiaz"/>
-                <meta name="format-detection" content="telephone=no"/>
-                <meta name="mobile-web-app-capable" content="yes"/>
-                <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
-                <meta name="msapplication-tap-highlight" content="no"/>
+        {/*These are need for PWA*/}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Muhammad Fiaz" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="msapplication-tap-highlight" content="no" />
 
-                <meta name="theme-color" content="#000000"/>
-                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-                <meta name="language" content="English"/>
-                <meta name="revisit-after" content="1 days"/>
-                <link rel="canonical" href="https://muhammadfiaz.com"/>
-                <meta name="license" content="MIT License"/>
-                <meta httpEquiv="content-language" content="en-us"/>
+        <meta name="theme-color" content="#000000" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <link rel="canonical" href="https://muhammadfiaz.com" />
+        <meta name="license" content="MIT License" />
+        <meta httpEquiv="content-language" content="en-us" />
 
-                <link rel="preconnect" href="https://muhammadfiaz.com"/>
+        <link rel="preconnect" href="https://muhammadfiaz.com" />
 
-                <link rel="dns-prefetch" href="https://muhammadfiaz.com"/>
+        <link rel="dns-prefetch" href="https://muhammadfiaz.com" />
 
-                <meta name="author" content="Muhammad Fiaz"/>
+        <meta name="author" content="Muhammad Fiaz" />
 
-                <link rel="alternate" hrefLang="en" href="https://muhammadfiaz.com"/>
+        <link rel="alternate" hrefLang="en" href="https://muhammadfiaz.com" />
 
-                <meta name="robots" content="index, follow"/>
-                <meta name="googlebot" content="index, follow"/>
-                <meta name="bingbot" content="index, follow"/>
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="bingbot" content="index, follow" />
 
-                <link rel="apple-touch-icon" href="/img/logo_rounded.png"/>
+        <link rel="apple-touch-icon" href="/img/logo_rounded.png" />
 
-                <link rel="apple-touch-icon" sizes="180x180" href="/img/logo_rounded.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.ico"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.jpg"/>
-                <link rel="icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
-                <link rel="shortcut icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/logo_rounded.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon.ico"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon.jpg"
+        />
+        <link
+          rel="icon"
+          href="/favicon/favicon-org.ico"
+          type="image/x-icon"
+        ></link>
+        <link
+          rel="shortcut icon"
+          href="/favicon/favicon-org.ico"
+          type="image/x-icon"
+        ></link>
 
-                <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
-                <meta name="msapplication-TileColor" content="#da532c"/>
-                <meta name="theme-color" content="#ffffff"/>
-                <link rel="manifest" href="/manifest.json"/>
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
 
+        <meta property="og:title" content="Muhammad Fiaz" key="title" />
+        <meta
+          property="og:description"
+          content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/75434191?v=4"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://avatars.githubusercontent.com/u/75434191?v=4"
+        />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:alt" content="Muhammad Fiaz" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url" content="https://muhammadfiaz.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:profile"
+          content="https://github.com/muhammad-fiaz"
+        />
+        <meta property="og:site_name" content="Muhammad Fiaz" />
+        <meta property="og:locale" content="en_US" />
 
-                <meta property="og:title" content="Muhammad Fiaz" key="title"/>
-                <meta property="og:description"
-                      content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey."/>
-                <meta property="og:image" content="https://avatars.githubusercontent.com/u/75434191?v=4"/>
-                <meta property="og:image:secure_url" content="https://avatars.githubusercontent.com/u/75434191?v=4"/>
-                <meta property="og:image:type" content="image/jpeg"/>
-                <meta property="og:image:alt" content="Muhammad Fiaz"/>
-                <meta property="og:image:width" content="300"/>
-                <meta property="og:image:height" content="300"/>
-                <meta property="og:url" content="https://muhammadfiaz.com"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:profile" content="https://github.com/muhammad-fiaz"/>
-                <meta property="og:site_name" content="Muhammad Fiaz"/>
-                <meta property="og:locale" content="en_US"/>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="https://twitter.com/muhammadfiaz_" />
+        <meta name="twitter:title" content="Muhammad Fiaz" />
+        <meta
+          name="twitter:description"
+          content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey."
+        />
+        <meta
+          name="twitter:image"
+          content="https://avatars.githubusercontent.com/u/75434191?v=4"
+        />
+        <meta
+          name="twitter:creator"
+          content="https://twitter.com/muhammadfiaz_"
+        />
+        <meta name="twitter:domain" content="https://muhammadfiaz.com" />
 
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="2048x2732"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="1668x2224"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="1536x2048"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="1125x2436"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="1242x2208"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="750x1334"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/logo_rounded.png"
+          sizes="640x1136"
+        />
 
-                <meta name="twitter:card" content="summary"/>
-                <meta name="twitter:site" content="https://twitter.com/muhammadfiaz_"/>
-                <meta name="twitter:title" content="Muhammad Fiaz"/>
-                <meta name="twitter:description"
-                      content="Muhammad Fiaz - A dedicated Full Stack Developer proficient in App/Web, Cloud, DevOps, AI/ML, and Design. Join my journey."/>
-                <meta name="twitter:image" content="https://avatars.githubusercontent.com/u/75434191?v=4"/>
-                <meta name="twitter:creator" content="https://twitter.com/muhammadfiaz_"/>
-                <meta name="twitter:domain" content="https://muhammadfiaz.com"/>
-
-
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='2048x2732'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1668x2224'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1536x2048'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1125x2436'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1242x2208'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='750x1334'/>
-                <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='640x1136'/>
-
-                {/*These are the Keywords that will Boost your SEO in Ranking, so Make Sure to include and Update it up to your preference or don't mind this! 😴*/}
-                <meta name="keywords" content="
+        {/*These are the Keywords that will Boost your SEO in Ranking, so Make Sure to include and Update it up to your preference or don't mind this! 😴*/}
+        <meta
+          name="keywords"
+          content="
     Muhammad Fiaz, Full Stack Engineer, Full Stack Developer, DevOps Engineer, Software Engineer,
     Programmer, Software Developer, Portfolio Website, Web Developer, Coding, Full Stack Development,
     Technology Enthusiast, Software Development, Computer Science, Programming Projects,
@@ -132,20 +205,21 @@ export default function Document() {
     open source contributions coding student,open source contributions coding enthusiast,open source contributions coding architect,
     open source contributions coding projects portfolio,open source contributions coding projects specialist,
 
-"/>
-                {/*use your own ads sense code here*/}
-		    <meta name="google-adsense-account" content="ca-pub-2040560600290490"/>
-		     <Script
+"
+        />
+        {/*use your own ads sense code here*/}
+        <meta name="google-adsense-account" content="ca-pub-2040560600290490" />
+        <Script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2040560600290490`}
           strategy="lazyOnload"
           crossOrigin="anonymous"
         />
-         
-                <script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{
-                        __html: `{
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
           "@context": "https://schema.org",
           "@type": "Person",
           "name": "Muhammad Fiaz",
@@ -157,14 +231,15 @@ export default function Document() {
             "https://twitter.com/muhammadfiaz_",
             "https://dev.to/muhammadfiaz"
           ]
-        }`
-                    }}
-                />
+        }`,
+          }}
+        />
 
-
-                {/*Edit this to your according FAQ */}
-                <script type="application/ld+json" dangerouslySetInnerHTML={{
-                    __html: `
+        {/*Edit this to your according FAQ */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
           {
             "@context": "https://schema.org",
             "@type": "FAQPage",
@@ -212,11 +287,14 @@ export default function Document() {
             ]
           }
         `,
-                }}/>
+          }}
+        />
 
-                {/*Edit this to your according to your website*/}
-                <script type="application/ld+json" dangerouslySetInnerHTML={{
-                    __html: `
+        {/*Edit this to your according to your website*/}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
           {
             "@context": "https://schema.org/",
             "@type": "BreadcrumbList",
@@ -247,12 +325,15 @@ export default function Document() {
               "item": "https://muhammadfiaz.com/signin"
             }]
           }
-        `
-                }}/>
+        `,
+          }}
+        />
 
-                {/*Edit this to your according to your website*/}
-                <script type="application/ld+json" dangerouslySetInnerHTML={{
-                    __html: `
+        {/*Edit this to your according to your website*/}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
           {
             "@context": "https://schema.org",
             "@type": "ResearchProject",
@@ -262,26 +343,30 @@ export default function Document() {
             "url": "https://github.com/muhammad-fiaz/neurolink",
             "logo": "https://private-user-images.githubusercontent.com/75434191/242214243-82fbf702-43e2-46e6-8e01-11758fa26310.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTQ4NTE2ODksIm5iZiI6MTY5NDg1MTM4OSwicGF0aCI6Ii83NTQzNDE5MS8yNDIyMTQyNDMtODJmYmY3MDItNDNlMi00NmU2LThlMDEtMTE3NThmYTI2MzEwLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA5MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwOTE2VDA4MDMwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ3NjJmNzA5MGU5NWY4NTUxZTcxYmVmNzNlZTYzOTViN2FjNzE1YTM2MjM1OTNlOGIxNzk0ODE1NDczMDMzNGEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.ay5bRLRoojQ33ZJLFnvjyq6Dq4qZfDY7quvv4WmgRLI",
              "sameAs": [
-            "https://www.linkedin.com/in/muhammad-fiaz-/"
+            "https://www.linkedin.com/in/fadihere/"
         ]          }
-        `
-                }}/>
-                {/*theme*/}
-                <Script id="theme.util.jsx" strategy="beforeInteractive">
-                    {`
+        `,
+          }}
+        />
+        {/*theme*/}
+        <Script id="theme.util.jsx" strategy="beforeInteractive">
+          {`
 				let themeLocalStorage = localStorage.getItem('theme')
 				let themeSystem       = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 				document.querySelector(':root').dataset.theme = themeLocalStorage ?? themeSystem
 				`}
-                </Script>
+        </Script>
 
-                {/* Google Analytics */}
-                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-SDJ0K1Y70X"/>
-                <Script
-                    id='google-analytics'
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SDJ0K1Y70X"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -289,14 +374,14 @@ export default function Document() {
             page_path: window.location.pathname,
           });
         `,
-                    }}
-                />
-            </Head>
+          }}
+        />
+      </Head>
 
-            <body>
-            <Main/>
-            <NextScript/>
-            </body>
-        </Html>
-    )
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
